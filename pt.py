ecd571452743df72e53067a5d7e999a27012d57f
NourHabhab <99543477+NourHabhab@users.noreply.github.com>
2022-08-05 16:46:51 +0300

    Update pt.py

--- pt.py
+++ pt.py
@@ -64,7 +64,7 @@ if menu_id == "Google":
     col1,col2,col3=st.columns([7,0.1,0.1])
     with col1:
         image1=Image.open('Capture9.PNG')
-        st.image(image1,width=1600)
+        st.image(image1,width=1580)
 
 #Editing Second Page of automated scrapper 
         
