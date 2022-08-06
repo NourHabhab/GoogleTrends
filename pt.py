64c3c34d302487449c7333babc53408f456aed99
NourHabhab <99543477+NourHabhab@users.noreply.github.com>
2022-08-06 17:20:34 +0300

    Update pt.py

--- pt.py
+++ pt.py
@@ -12,7 +12,6 @@ import requests
 
 
 
-
 #Layout 
 st.set_page_config(layout="wide", page_icon = "https://cdn-icons-png.flaticon.com/512/2702/2702602.png", page_title = "Google Trends")
 st.expander('Expander') 
@@ -20,7 +19,7 @@ st.expander('Expander')
 
 #creating menu data which will be used in navigation bar specifying the pages of the dashboard
 menu_data = [
-{'label':"Google",'icon': "bi bi-google"},
+{'label':"Google"},
 {'label':"1",'icon': "bi bi-google"},
 {'label':"2",'icon': "bi bi-google"},
 {'label':'3','icon': "bi bi-google"}]
@@ -40,14 +39,16 @@ menu_id = hc.nav_bar(menu_definition = menu_data,
 if menu_id == "Google":
     #Installing an image 
     image = Image.open('1.PNG')
-
     row_spacer1, row_1, row_spacer2, row_2 = st.columns((.1, .1, .3, 1.8))
     with row_spacer1:
             st.image(image,width=300)
     with row_spacer2:
             st.empty()
     #Segmenting the page for the desired layout 
-
+    
+    # Display lottie animations
+ 
+  
     col1,col2,col3=st.columns([1,2,1])
     with col2:
         st.markdown(f"""
@@ -57,14 +58,15 @@ if menu_id == "Google":
             </h1>""",unsafe_allow_html = True)
         
     col1,col2,col3,col4,col5,col6=st.columns([1,1.3,1,1,1,1])
+    
     with col3:
         image1=Image.open('Capture1.PNG')
         st.image(image1,width=600)
         
     col1,col2,col3=st.columns([7,0.1,0.1])
     with col1:
-        image1=Image.open('Capture9.PNG')
-        st.image(image1,width=1500)
+        image1=Image.open('homecap.png')
+        st.image(image1,width=1650)
 
 #Editing Second Page of automated scrapper 
         
@@ -76,17 +78,9 @@ if menu_id == "1":
             st.image(image,width=400)
     with row_spacer2:
             st.empty()
-             
-    col1,col2,col3,col4=st.columns([4,0.2,0.2,4])
-    #Summary of what the page represents 
-    with col1:
-        st.markdown(f"""
-                    <h4>
-                        <h4 style="vertical-align:left;font-size:25px;color:#4285F4;padding-left:200px;padding-top:5px;margin-left:0em";>
-                        In this page, we will explore the trend of a single keyword within a specific region to optimize our SEO
-                    </h4>""",unsafe_allow_html = True)
-    #This is a dictonary representing the code of the geoloction in google trends 
-    with col4:
+            
+    col=st.columns(2)
+    with col[0]:
         def load_lottieurl(url):
 
                 # get the url
@@ -96,10 +90,14 @@ if menu_id == "1":
                 return None
             return r.json()
     
-        lottie_google = load_lottieurl("https://assets8.lottiefiles.com/packages/lf20_dhlmjljy.json")
-        st_lottie(lottie_google, key = "google", height = 200, width = 580)
+        lottie_google = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_xh83pj1c.json")
+        st_lottie(lottie_google, key = "google", height = 300, width = 900)
+    with col[1]:
+        image1=Image.open('Chat1.PNG')
+        st.image(image1,width=600)
+    
+   
 
-    #This is a dictonary representing the code of the geoloction in google trends 
    
     countries1={'United States':"US",'United Arab Emirates':"AE","Germany":"DE","Afghanistan":"AF",
                 "Åland Islands":"AX","Albania":"AL","Algeria":"DZ","American Samoa":"AS","Andorra":"AD","Angola":"AO","Anguilla":"AI","Antarctica":"AQ","Argentina":"AR",
@@ -169,7 +167,6 @@ if menu_id == "1":
                     d1  = pytrends.interest_over_time()
                     d1.to_csv('MTrends.csv')
                     df1=pd.read_csv("MTrends.csv")
-                   
                     df1['date'] = pd.to_datetime(df1['date'])
                     df1['date'] = df1['date'].apply(lambda row: row.year)
                     pivot1 = df1.pivot_table(index=['date'], values=list2, aggfunc='mean')
@@ -200,7 +197,7 @@ if menu_id == "1":
                     st.plotly_chart(fig2, use_container_width=True)
 
 #Editing Third Page of automated scrapper   
-if menu_id == "2":
+if menu_id == "3":
     image = Image.open('Capture6.PNG')
 
     row_spacer1, row_1, row_spacer2, row_2 = st.columns((.1, .1, .3, 1.8))
@@ -208,18 +205,8 @@ if menu_id == "2":
             st.image(image,width=400)
     with row_spacer2:
             st.empty()
-    
-    
-    col1,col2,col3,col4=st.columns([6,0.2,0.2,4])
-    #Summary of what the page represents 
-    with col1:
-        st.markdown(f"""
-                    <h3>
-                        <h4 style="vertical-align:center;font-size:25px;color:#4285F4;padding-left:200px;padding-top:5px;margin-left:0em";>
-                        In this page, a new feature for google trends has been added! We can compare the search term in two different countries.
-                    </h3>""",unsafe_allow_html = True)
-    #This is a dictonary representing the code of the geoloction in google trends 
-    with col4:
+    col=st.columns(2)
+    with col[0]:
         def load_lottieurl(url):
 
                 # get the url
@@ -229,8 +216,14 @@ if menu_id == "2":
                 return None
             return r.json()
     
-        lottie_countries = load_lottieurl("https://assets1.lottiefiles.com/packages/lf20_HLo5AP.json")
-        st_lottie(lottie_countries, key = "countries", height = 200, width = 700)
+        lottie_google = load_lottieurl("https://assets7.lottiefiles.com/private_files/lf30_uetcyfgk.json")
+        st_lottie(lottie_google, key = "google", height = 300, width = 900)
+    with col[1]:
+        image1=Image.open('Chat2.PNG')
+        st.image(image1,width=600)
+    
+
+  
     
     #This is a dictonary representing the code of the geoloction in google trends 
     
@@ -412,7 +405,7 @@ if menu_id == "2":
                     st.plotly_chart(fig3, use_container_width=True)
 
 #Editing Fourth Page of automated scrapper  
-if menu_id=="3": 
+if menu_id=="2": 
     image = Image.open('Capture6.PNG')
 
     row_spacer1, row_1, row_spacer2, row_2 = st.columns((.1, .1, .3, 1.8))
@@ -421,17 +414,8 @@ if menu_id=="3":
     with row_spacer2:
             st.empty()
     
-   
-    col1,col2,col3,col4=st.columns([6,0.2,0.2,4])
-    #Summary of what the page represents 
-    with col1:
-        st.markdown(f"""
-                    <h3>
-                        <h4 style="vertical-align:center;font-size:25px;color:#4285F4;padding-left:200px;padding-top:5px;margin-left:0em";>
-                         In this page, we will explore the trend of two keywords within a specific region to optimize our SEO.
-                    </h3>""",unsafe_allow_html = True)
-    #This is a dictonary representing the code of the geoloction in google trends 
-    with col4:
+    col=st.columns(2)
+    with col[0]:
         def load_lottieurl(url):
 
                 # get the url
@@ -441,8 +425,15 @@ if menu_id=="3":
                 return None
             return r.json()
     
-        lottie_SEO = load_lottieurl("https://assets6.lottiefiles.com/packages/lf20_d5xpsakx.json")
-        st_lottie(lottie_SEO, key = "SEO", height = 200, width = 500)
+        lottie_google = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_xh83pj1c.json")
+        st_lottie(lottie_google, key = "google", height = 300, width = 900)
+    with col[1]:
+        image1=Image.open('Chat3.PNG')
+        st.image(image1,width=600)
+
+    
+   
+    
         
     pytrends=TrendReq(hl='en-Us')    
     placeholder=st.empty()
